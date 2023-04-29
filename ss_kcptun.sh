b6075f0a3d96d82ac86c2c0c79e57c9816ebefba
Dear.free <62785542+future-death@users.noreply.github.com>
2023-04-29 23:09:56 +0800

    Delete ss_kcptun.sh

--- ss_kcptun.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-python get-pip.py
-pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
-wget https://github.com/xtaci/kcptun/releases/download/v20210922/kcptun-linux-amd64-20210922.tar.gz
-tar -zxvf kcptun-linux-amd64-20210922.tar.gz
-ssserver -c /root/ss-and-kcptun/ssconfig.json -d start
-nohup ./server_linux_amd64 -c "/root/ss-and-kcptun/kcpconfig.json" &
